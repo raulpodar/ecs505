2174e2c68dfe0df3d7c7d34dcc2aa63806d9148f
raulpodar <raulpodar0@gmail.com>
2020-10-06 12:15:19 +0100

    first commint

---
+++ src/ecs505/Ecs505.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ecs505;
+
+/**
+ *
+ * @author raulp
+ */
+public class Ecs505 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
