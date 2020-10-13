f725f43b2ddbb6168ad89e7aba1a85945322457d
raulpodar <raulpodar0@gmail.com>
2020-10-13 08:29:36 +0100

    push to upstream

--- src/ecs505/Main.java
+++ src/ecs505/Main.java
@@ -14,5 +14,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("My first Git repo");
         System.out.println("me new branch");
+        System.out.println("push to upstream boi");
     }
 }
